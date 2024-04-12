920b6a03f77203cf86c0ca63d71a4eea574b7b62
georgi2005atanasov <gogida9876@abv.bg>
2024-04-12 22:08:18 +0300

    Made the filters button in the sidebar look better.

--- frontend/nutri-best-project/src/pages/products/AllProducts.jsx
+++ frontend/nutri-best-project/src/pages/products/AllProducts.jsx
@@ -6,6 +6,7 @@ import Pagination from "../../components/UI/Pagination";
 import SideBar from "../../components/UI/SideBar";
 import SideBarToggler from "../../components/UI/SideBarToggler";
 import CategoryContextProvider from "../../store/CategoryContext";
+import styles from "../css/AllProducts.module.css";
 
 const PRODUCTS_PER_PAGE = 6;
 const PRODUCTS_PER_ROW = 3;
@@ -31,8 +32,8 @@ export default function AllProducts() {
         <div className="all-products d-flex justify-content-end">
             <div className="container">
                 <div className="row d-flex flex-md-column justify-content-center">
-                    <div className="row d-flex justify-content-center align-items-start">
-                        <div className="col-md-3 d-flex flex-column">
+                    <div className="row d-flex justify-content-between align-items-start">
+                        <div className={`${styles["filters"]} col-md-3 d-flex flex-column}`}>
                             <SideBarToggler toggleSidebar={toggleSidebar} />
                             <SideBar isVisible={isSidebarVisible} toggleSidebar={toggleSidebar} />
                         </div>

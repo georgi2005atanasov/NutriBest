877c5d2c67a9a0d92a19c526c7ea212637a3167f
georgi2005atanasov <gogida9876@abv.bg>
2024-04-20 11:54:27 +0300

    Made better design of the error page. (can be always better).

--- frontend/nutri-best-project/src/pages/Error.jsx
+++ frontend/nutri-best-project/src/pages/Error.jsx
@@ -1,4 +1,13 @@
+import styles from "./css/Error.module.css";
+import errorImage from "../assets/error.png";
+
 // eslint-disable-next-line react/prop-types
 export default function ErrorPage({ code }) {
-    return <h1>Error {code}!</h1>
+    return <div className={`${styles["container-err"]}`}>
+        <h1 className={`${styles["wrong"]}`}>Oops! Something went wrong.</h1>
+        <p className={`${styles["trouble"]}`}>We're having trouble loading the page you requested.</p>
+        <img src={errorImage} alt="Error Icon" className={`${styles["error-icon"]}`} />
+            <button className={`${styles["back-btn"]}`} onClick={() => history.back()}>Go Back</button>
+            <p>If the problem persists, please <a href="mailto:support@supplementsapp.com">contact us</a>.</p>
+    </div >
 }

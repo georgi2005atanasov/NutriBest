974e7c0088b80eb0b6fe94ec49ad6eb373fa4737
georgi2005atanasov <gogida9876@abv.bg>
2024-05-03 01:16:36 +0300

    Removed the ugly squares behind the dropdowns in the filters.

--- frontend/nutri-best-project/src/components/UI/Dropdown/DropdownMenu.jsx
+++ frontend/nutri-best-project/src/components/UI/Dropdown/DropdownMenu.jsx
@@ -10,31 +10,39 @@ export default function DropdownMenu({ children, text, filtersNumber = 0 }) {
         setShowMenu(!showMenu);
     }
 
-    return <motion.div
-        className={styles["dropdown"]}
-    >
-        <button type={"button"} onClick={toggleMenu} className={`${styles["dropdown-button"]}`}>
-            {text}
-        </button>
-        {filtersNumber != 0 ?
-            <div className={styles["circle"]}>{filtersNumber}</div> :
-            ""}
+    return <AnimatePresence>
+        <motion.div
+            className={styles["dropdown"]}
+            initial={{ scaleY: 0 }}
+            animate={{ scaleY: 1 }}
+            exit={{ scaleY: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            style={{
+                transformOrigin: 'top'
+            }}
+        >
+            <button type={"button"} onClick={toggleMenu} className={`${styles["dropdown-button"]}`}>
+                {text}
+            </button>
+            {filtersNumber != 0 ?
+                <div className={styles["circle"]}>{filtersNumber}</div> :
+                ""}
 
-        {showMenu ? (<AnimatePresence>
-            <motion.div
-                className={styles["dropdown-menu"]}
-                initial={{ scaleY: 0 }}
-                animate={{ scaleY: 1 }}
-                exit={{ scaleY: 0 }}
-                transition={{ duration: 0.5, ease: "easeInOut" }}
-                style={{
-                    overflow: 'hidden',
-                    transformOrigin: 'top'
-                }}
-            >
-                {children}
-            </motion.div>
-        </AnimatePresence>
-        ) : undefined}
-    </motion.div>
+            {showMenu ? (
+                <motion.div
+                    className={styles["dropdown-menu"]}
+                    initial={{ scaleY: 0 }}
+                    animate={{ scaleY: 1 }}
+                    exit={{ scaleY: 0 }}
+                    transition={{ duration: 0.3, ease: "easeOut" }}
+                    style={{
+                        overflow: 'hidden',
+                        transformOrigin: 'top'
+                    }}
+                >
+                    {children}
+                </motion.div>
+            ) : undefined}
+        </motion.div>
+    </AnimatePresence>
 }
--- frontend/nutri-best-project/src/components/UI/Sidebar/SideBarToggler.jsx
+++ frontend/nutri-best-project/src/components/UI/Sidebar/SideBarToggler.jsx
@@ -5,10 +5,6 @@ import { motion } from "framer-motion";
 export default function SideBarToggler({ toggleSidebar }) {
     return <motion.div
         className="d-flex justify-content-start w-100 ms-4"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 20 }}
-        transition={{ duration: 0.4 }}
     >
         <button onClick={toggleSidebar} className={`${styles["sidebar-toggler"]} ms-5 w-100 align-items-center justify-content-between d-flex d-xl-none mb-1`}>
             Filters <i className="fa fa-filter" aria-hidden="true"></i>

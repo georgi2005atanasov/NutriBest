bcfb6efb2e8d94c100468c5ec1fe4c1999c078d1
georgi2005atanasov <gogida9876@abv.bg>
2024-06-11 14:43:52 +0300

    Removed some bugs when rendering the messages to the admin.

--- frontend/nutri-best-project/src/components/Notifications/Notification.jsx
+++ frontend/nutri-best-project/src/components/Notifications/Notification.jsx
@@ -1,33 +1,41 @@
+import { registerNotificationHandler } from '../../../../../backend/services/signalRService';
+import styles from './css/Notification.module.css';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import { onReceiveNotification, registerNotificationHandler } from '../../../../../backend/services/signalRService';
 
 function Notification() {
     const [notifications, setNotifications] = useState([]);
 
     const handleNotifyAdmin = (notificationType, message) => {
-        setNotifications(prevNotifications => [...prevNotifications, { type: notificationType, message }]);
+        setNotifications(prevNotifications => [...prevNotifications, 
+            !prevNotifications.some(x => x.message == message) && 
+            { type: notificationType, message }]);
+
+        setTimeout(() => {
+            setNotifications([]);
+        }, 5000);
     };
 
     useEffect(() => {
-        onReceiveNotification((type, message) => {
-            setNotifications(prevNotifications => [...prevNotifications, { type, message }]);
-        });
-
         registerNotificationHandler(handleNotifyAdmin);
     }, []);
 
-    
-
     return (
-        <div className="NotificationComponent">
-            <h2>Notifications</h2>
-            <ul>
-                {notifications.map((notification, index) => (
-                    <li key={index}>
-                        <strong>{notification.type}: </strong>{notification.message}
-                    </li>
+        <div className={`${styles["notification-container"]} ms-2 mt-3 position-fixed d-flex justify-content-center align-items-center`}>
+            <AnimatePresence>
+                {notifications.filter(x => x.message).map(({ id, message }) => (
+                    <motion.div
+                        key={id}
+                        initial={{ opacity: 0, y: 50, scale: 0.3 }}
+                        animate={{ opacity: 1, y: 0, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
+                        transition={{ type: "spring", stiffness: 260, damping: 20 }}
+                        className={styles["notification"]}
+                    >
+                        <strong>{message}</strong>
+                    </motion.div>
                 ))}
-            </ul>
+            </AnimatePresence>
         </div>
     );
 }
